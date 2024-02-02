4c38b1eb1318a455fe3f0da123be28014d8d13d4
AnasNAK <anasc00ding@gmail.com>
2024-02-02 07:56:07 +0100

    update in route file

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\Adventure;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+// Route::get('home/{recit}',function($test){
+
+//     $path = __DIR__ . "/../resources/adventures/{$test}.html";
+//     if(!file_exists($path)){
+// dd('we have a biig problem ');
+
+//     }
+
+//     $recit = file_get_contents($path);
+
+// return view('Adventure' , [
+//     'recit' => $recit
+// ]);
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('Home',[
+        'adventures' => adventure::all()
+    ]);
+});
+
+
+Route::get('/home/{adventure}',function(Adventure $adventure){
+
+return view('Adventure',[
+    'adventure' => $adventure
+    ]);
+
+
 });
